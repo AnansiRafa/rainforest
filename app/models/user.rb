ea86c1a3ae4610dd1af770abc984dc66b60978bd
Anthony Cook <antrafcook@gmail.com>
2018-03-17 16:22:10 -0400

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  valid_email = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates(:name, presence: true, length: { minimum: 2, maximum: 25  })
+  validates :email, presence: true, format: { with: valid_email }, uniqueness: { case_sensitive: false}
 end
