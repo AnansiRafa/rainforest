5c2abfd866668e58959feb48bf1a7b5b229a95f6
Anthony Cook <antrafcook@gmail.com>
2018-03-17 16:57:04 -0400

    made changes to User model, added index to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   valid_email = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates(:name, presence: true, length: { minimum: 2, maximum: 25  })
-  validates :email, presence: true, format: { with: valid_email }, uniqueness: { case_sensitive: false}
+  validates :email, presence: true, format: { with: valid_email }, length: {minimum: 5, maximum: 25}, uniqueness: { case_sensitive: false}
+
+  has_secure_password
 end
