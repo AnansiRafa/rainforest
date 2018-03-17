96e4210de99489f537018daa4caf45781b794a28
Anthony Cook <antrafcook@gmail.com>
2018-03-17 18:45:26 -0400

    more adjustments to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
+
   valid_email = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates(:name, presence: true, length: { minimum: 2, maximum: 25  })
-  validates :email, presence: true, format: { with: valid_email }, length: {minimum: 5, maximum: 25}, uniqueness: { case_sensitive: false}
+
+  validates(:email, presence: true, format: { with: valid_email }, length: {minimum: 5, maximum: 25}, uniqueness: { case_sensitive: false})
+
+  validates :password, presence: true, length: { minimum: 6 }
 
   has_secure_password
 end
